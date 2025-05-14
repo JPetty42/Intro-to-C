ab547a1b87b565cbfa053cb87431fcacf6863c06
JPetty42 <156225147+JPetty42@users.noreply.github.com>
2025-05-14 12:45:07 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,5 +5,7 @@ int myArray[] = {0, 2, 3, 4,5};
 
 int main(void) {
   printf("Welcome to data types!");
-  printf("Third value of myArray is %d. \n", myArray[2] 
+  printf("Third value of myArray is %d. \n", myArray[2]);
+  printf("The array is %d bytes long. \n", sizeof(myArray));
+    
 }
